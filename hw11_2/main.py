b40e276b1f293b39356895a5d0308f51b07c08c7
Ivan <ifucin@gmail.com>
2022-04-25 18:00:16 +0300

    Первая и вторая часть домашней работы

---
+++ hw11_2/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from utils import load_candidates_from_json, get_candidate, get_candidates_by_name, get_candidates_by_skill
+
+app = Flask(__name__)
+
+data = load_candidates_from_json('candidates.json')
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', candidates=data)
+
+@app.route("/candidate/<int:uid>")
+def profile(uid):
+    candidate = get_candidate(uid)
+    return render_template('profile.html', candidates=candidate)
+
+@app.route("/search/<name>")
+def search(name):
+    candidate = get_candidates_by_name(name)
+    return render_template('search.html', candidates=candidate, candidates_len=len(candidates))
+
+@app.route("/skills/<skill>")
+def get_skills(skill):
+    candidate = get_candidates_by_skill(skill)
+    return render_template('skills.html', candidates=candidate, candidates_len=len(candidates), skill=skill)
+
+app.run()
---
+++ hw11_2/utils.py
@@ -0,0 +1,45 @@
+import json
+
+__data = []
+
+
+def load_candidates_from_json(path):
+    """
+
+    :return:
+    """
+    global __data
+    with open(path, 'r', encoding='utf-8') as file:
+        __data = json.load(file)
+    return __data
+
+
+def get_candidate(candidate_id):
+    """
+Ищет кандидата по ID и возвращает словарь с информацие о кандидате
+
+    """
+    for candidate in __data:
+        if candidate['id'] == candidate_id:
+            return {
+                'name': candidate['name'],
+                'position': candidate['position'],
+                'picture': candidate['picture'],
+                'skills': candidate['skills'],
+            }
+    return {'not_found': 'не найдено'}
+
+def get_candidates_by_name(candidate_name: str):
+    """
+    Ищет ккандидатов по имени и возврвщвет список со словарями найденых кандидатов
+    через списковое включение
+    """
+    return [candidate for candidate in __data if candidate_name.lower() in candidate['name'].lower()]
+
+
+def get_candidates_by_skill(skill_name: str):
+    candidates = []
+    for candidate in __data:
+        skills = candidate['skills'].lower().split(', ')
+        if skill_name.lower() in skills:
+            candidates.append(candidate)
